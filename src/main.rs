e236f439489b58ef15a7b2bbd70cccde6351169b
Philippe LE <le.philippe.duc.tai@gmail.com>
2024-12-25 19:44:01 +0100

    Added next section.

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,17 @@ impl Plugin for HelloPlugin {
         );
     }
 }
+#[derive(Resource)]
+struct GreetTimer(Timer);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
 
 fn main() {
     App::new()
